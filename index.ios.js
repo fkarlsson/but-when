b60f9cc8dca83683d5945452f9928c3b20315cf5
Fredrik Karlsson <fredrik.karlsson@nepartner.se>
2015-04-07 23:43:37 +0200

    Added some scaffolding and a very simple song by year fetcher

--- index.ios.js
+++ index.ios.js
@@ -10,23 +10,64 @@ var {
   StyleSheet,
   Text,
   View,
+  Image
 } = React;
 
 var butwhen = React.createClass({
   render: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+Control+Z for dev menu
-        </Text>
+        <Player />
       </View>
+    );
+  }
+});
+
+var Player = React.createClass({
+  getInitialState: function() {
+    return {
+      year: 1920,
+      href: '',
+      albumArt: 'http://placehold.it/380x380',
+      playing: false
+    };
+  },
+  fetchNewSong: function() {
+    var year = Math.round(1920 + 95 * Math.random());
+
+    fetch('https://api.spotify.com/v1/search?q=year:' + year + '&type=track')
+      .then((response) => response.json())
+      .then((responseData) => {
+        var track = responseData.tracks.items[Math.round(10 * Math.random())];
+        var image = track.album.images[1];
+        
+        this.setState({
+          year: year,
+          href: track.href,
+          albumArt: image.url
+        });
+      })
+      .done();
+  },
+  componentDidMount: function() {
+    this.fetchNewSong();
+  },
+  render: function() {
+    return (
+      <View style={styles.player}>
+        <Image style={playerStyles.albumArt} source={{uri: this.state.albumArt}}>
+          <Text style={playerStyles.text}>Player</Text>
+          <Controls />
+        </Image>
+      </View>
+    );
+  }
+});
+
+var Controls = React.createClass({
+  render: function() {
+    return (
+      <Text style={playerStyles.text}>Play</Text>
     );
   }
 });
@@ -48,6 +89,16 @@ var styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+});
+
+var playerStyles = StyleSheet.create({
+  text: {
+    backgroundColor: 'transparent'
+  },
+  albumArt: {
+    width: 380,
+    height: 380
+  }
 });
 
 AppRegistry.registerComponent('butwhen', () => butwhen);
